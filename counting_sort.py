fad1499a56c75a0ebb03d2e19dd33a7092956249
AnilMeena1512 <72185078+AnilMeena1512@users.noreply.github.com>
2021-07-14 18:53:48 +0530

    Create counting_sort.py

---
+++ counting_sort.py
@@ -0,0 +1,18 @@
+def counting_sort(array):
+    size=len(array)
+    output=[0]*size
+    count=[0]*10
+    
+    for i in range(0,size):
+        count[array[i]]+=1
+    for i in range(1,10):
+        count[i]+=count[i-1]
+    i=size-1
+    
+    while i>=0:
+        output[count[array[i]]-1]=array[i]
+        count[array[i]]-=1
+        i-=1
+    return(output)
+array=list(map(int,input().split()))
+print(counting_sort(array))
